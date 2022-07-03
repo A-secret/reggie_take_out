34dc86832360c6ebcf0f23f3749ab7bdbb107a0f
zwj <469585132@qq.com>
2022-07-03 12:42:52 +0800

    Initial commit

--- src/main/java/com/itheima/reggie/controller/CommonController.java
+++ src/main/java/com/itheima/reggie/controller/CommonController.java
@@ -1,6 +1,7 @@
 package com.itheima.reggie.controller;
 
 import com.itheima.reggie.common.R;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,6 +17,12 @@ import org.springframework.web.multipart.MultipartFile;
 @RequestMapping("/common")
 public class CommonController {
 
+    /**
+     * 文件上传
+     * @param file
+     * @return
+     */
+    @PostMapping("/upload")
     public R<String> upload(MultipartFile file){
          return null;
     }
