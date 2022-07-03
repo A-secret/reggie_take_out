20c90b5d8df5ad1029d5c53fbe3cb16bebae6cfd
zwj <469585132@qq.com>
2022-07-03 17:21:04 +0800

    Initial commit

--- src/main/java/com/itheima/reggie/controller/CommonController.java
+++ src/main/java/com/itheima/reggie/controller/CommonController.java
@@ -1,6 +1,7 @@
 package com.itheima.reggie.controller;
 
 import com.itheima.reggie.common.R;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,6 +16,7 @@ import org.springframework.web.multipart.MultipartFile;
  */
 @RestController
 @RequestMapping("/common")
+@Slf4j
 public class CommonController {
 
     /**
@@ -24,6 +26,7 @@ public class CommonController {
      */
     @PostMapping("/upload")
     public R<String> upload(MultipartFile file){
+        log.info(file.toString());
          return null;
     }
 }
